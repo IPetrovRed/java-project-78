819090e3dbaad83a590dbe07d6f5004ba9fa82b5
IPetrovRed <ipetrovdat@gmail.com>
2024-05-09 20:00:19 +0300

    3-rd attempt to pass hexlet tests

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
+import hexlet.code.schemas.BaseSchema;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
