74a97c312c27a43fc418ea1d77a812b665362d8b
IPetrovRed <ipetrovdat@gmail.com>
2024-05-09 20:21:04 +0300

    7-th attempt to pass hexlet tests

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -5,7 +5,6 @@ import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
 
 public class Validator {
-
     public final StringSchema string() {
         return new StringSchema();
     }
@@ -18,3 +17,4 @@ public class Validator {
         return new MapSchema();
     }
 }
+
