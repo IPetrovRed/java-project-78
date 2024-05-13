b9c788c1a861403f34ab957fdfd620a7c7dce237
IPetrovRed <ipetrovdat@gmail.com>
2024-05-13 22:57:22 +0300

    check for errors on hexlet

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,26 +2,23 @@ package hexlet.code.schemas;
 
 import java.util.Map;
 
-public final class MapSchema extends BaseSchema {
-
-    public MapSchema required() {
+public final class MapSchema extends BaseSchema<Map> {
+    public void required() {
         addTest(value -> value instanceof Map);
-        return this;
     }
 
-    public MapSchema sizeof(Integer sizeof) {
-        addTest(value -> ((Map) value).size() == sizeof);
-        return this;
+    public void sizeof(Integer sizeof) {
+        addTest(value -> value.size() == sizeof);
     }
 
-    public MapSchema shape(Map<String, BaseSchema> shape) {
+    public void shape(Map<String, BaseSchema> shape) {
         addTest(value -> {
             return shape.keySet().stream()
                     .map((key) -> {
-                        return shape.get(key).isValid(((Map) value).get(key));
+                        return shape.get(key).isValid(value.get(key));
                     })
                     .allMatch((isValid) -> isValid);
         });
-        return this;
     }
 }
+
--- app/src/test/java/hexlet/code/schemas/StringTest.java
+++ app/src/test/java/hexlet/code/schemas/StringTest.java
@@ -5,30 +5,21 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StringTest {
-
     @Test
     void stringIsValid() {
         Validator v = new Validator();
         StringSchema schema = v.string();
-
         assertThat(schema.isValid("")).isTrue();
-
         schema.required();
         assertThat(schema.isValid("what does the fox say")).isTrue();
         assertThat(schema.isValid("hexlet")).isTrue();
         assertThat(schema.isValid("")).isFalse();
         assertThat(schema.isValid(null)).isFalse();
-
         schema.minLength(7);
         assertThat(schema.isValid("what does the fox say")).isTrue();
         assertThat(schema.isValid("hexlet")).isFalse();
-
-        assertThat(
-                schema.contains("what").isValid("what does the fox say")
-        ).isTrue();
-
-        assertThat(
-                schema.contains("whatthe").isValid("what does the fox say")
-        ).isFalse();
+        assertThat(schema.contains("what").isValid("what does the fox say")).isTrue();
+        assertThat(schema.contains("whatthe").isValid("what does the fox say")).isFalse();
     }
 }
+
