c5c21ab495b0539ed11fc31f30007e735ec1cea0
IPetrovRed <ipetrovdat@gmail.com>
2024-05-14 20:54:01 +0300

    a.a. to pass tests #8

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -5,8 +5,8 @@ import java.util.Map;
 public final class MapSchema extends BaseSchema<Map<?, ?>> {
 
     public MapSchema shape(Map<String, BaseSchema> schemas) {
-        addCheck("name", s -> schemas.get("name").isValid(s.get("name")));
-        addCheck("age", s -> schemas.get("age").isValid(s.get("age")));
+        addCheck("name", s -> schemas.get("name").isValid(((Map) s).get("name")));
+        addCheck("age", s -> schemas.get("age").isValid(((Map) s).get("age")));
         return this;
     }
 
