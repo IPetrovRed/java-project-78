0d655fea5fd31357319d1255ab5cb792a59f5c84
IPetrovRed <ipetrovdat@gmail.com>
2024-05-14 19:41:10 +0300

    BaseSchema changes to pass tests

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -6,19 +6,18 @@ import java.util.Objects;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema<T> {
-    protected boolean required;
-    protected Map<String, Predicate<T>> checks = new LinkedHashMap<>();
+    boolean required;
+    Map<String, Predicate<T>> checks = new LinkedHashMap<>();
 
     public void addCheck(String checkName, Predicate<T> testCheck) {
         checks.put(checkName, testCheck);
     }
 
     public boolean isValid(T objectForValidation) {
-
         if (Objects.equals(objectForValidation, null)) {
             return !required;
         }
-        for (String key: checks.keySet()) {
+        for (String key : checks.keySet()) {
             if (!checks.get(key).test(objectForValidation)) {
                 return false;
             }
