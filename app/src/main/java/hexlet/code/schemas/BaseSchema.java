7527139e511a77541c6ff82d74606aa4a2db0263
IPetrovRed <ipetrovdat@gmail.com>
2024-05-14 20:11:20 +0300

    a.a. to pass tests #03

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 
-public class BaseSchema<T> {
+public abstract class BaseSchema<T> {
     boolean required;
     Map<String, Predicate<T>> checks = new LinkedHashMap<>();
 
