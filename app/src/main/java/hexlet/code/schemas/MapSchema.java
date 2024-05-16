8bd28ac99ee9d339a629ab4df8b765c210885e6b
IPetrovRed <ipetrovdat@gmail.com>
2024-05-16 20:01:24 +0300

    MapSchema changes in schema chek

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -5,8 +5,16 @@ import java.util.Map;
 public final class MapSchema<T> extends BaseSchema<Map<String, T>> {
 
     public MapSchema shape(Map<String, BaseSchema<T>> schemas) {
-        schemas.forEach((key, schema) ->
-                addCheck(key, s -> schema.isValid(s.get(key)))
+        addCheck(
+                "shape",
+                map -> {
+                    for (Map.Entry<String, BaseSchema<T>> entry : schemas.entrySet()) {
+                        if (!entry.getValue().isValid(map.get(entry.getKey()))) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
         );
         return this;
     }
